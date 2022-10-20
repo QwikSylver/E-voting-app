38d7572b443a90020e8bf5db8a48ae8a79ef52a9
Sakaria Nghivafe <snghivafe@gmail.com>
2022-10-20 17:45:17 +0200

    Created Models

--- app/app/settings.py
+++ app/app/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "evoting",
+        "USER": "voting",
+        "PASSWORD": "Password123.",
+        "HOST": "localhost",
+        "PORT": "3306",
+        "sql_mode": "STATIC_TRANS_TABLES",
     }
 }
 
---
+++ app/elections/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    category_reason = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.election_reason
+
+class Election(models.Model):
+    election_id = models.AutoField(primary_key=True)
+    topic = models.CharField(max_length=100)
+    elections = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.topic
+
+class Candidate(models.Model):
+    candidate_id = models.AutoField(primary_key=True)
+    candidate_name = models.CharField(max_length=100)
+    candidate_election = models.ForeignKey(Election, on_delete=models.CASCADE)
+    description = models.CharField(max_length=250, blank=True, null=True)
+
+    def __str__(self):
+        return self.candidate_name
+
+class Voter(models.Model):
+    voter_id = models.AutoField(primary_key=True)
+    voter_password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.voter_id
+
+class Vote(models.Model):
+    voter_id = models.ForeignKey(Voter, on_delete=models.CASCADE)
+    election_id = models.ForeignKey(Election, on_delete=models.CASCADE)
+    candidate_id = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.vote_id
