892567444b63719d8f8bca74a9d44fd2567bd5d2
Sakaria Nghivafe <snghivafe@gmail.com>
2022-10-22 10:49:10 +0200

    Implemented Page Restriction

--- app/elections/urls.py
+++ app/elections/urls.py
@@ -5,10 +5,14 @@ from .views import (
     electionsPage,
     electionDetails,
     categoryDetails,
+    logoutUser,
+    loginPage,
 )
 
 
 urlpatterns = [
+    path('login/', loginPage, name='login'),
+    path('logout/', logoutUser, name='logout'),
     path("elections/", electionsPage, name="home"),
     path("elections/<str:election_id>", electionDetails, name="elections_details"),
     path("user_profile/<str:user_id>", UserProfileView, name="user_profile"),
--- app/elections/views.py
+++ app/elections/views.py
@@ -1,35 +1,65 @@
+# from urllib import request
 from django.http import HttpResponse, Http404
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required 
 from django.contrib.auth import authenticate, login, logout
 from .models import *
 
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
-def HomePageView(response):
-    return HttpResponse("Home Page")
+def UserProfileView(request, user_id):
 
-
-def UserProfileView(response, user_id):
+    if request.user.is_authenticated:
+        return redirect('home')
     try:
         user = Voter.objects.get(voter_id=user_id)
     except Voter.DoesNotExist:
         raise Http404("Voter does not exist")
-    return render(response, "elections/user_profile.html", {"user": user})
+    return render(request, "elections/user_profile.html", {"user": user})
 
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
 
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = Voter.objects.get(voter_id=username)
+        except:
+            messages.error(request, 'Incorrect Username or Password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, 'Account is disabled')
+        else:
+            messages.error(request, 'Username or Password Does Not Exist')
+
+    return render(request, 'registration/login.html', {})
+
+@login_required(login_url='login')
 def electionsPage(request):
     elections = Election.objects.all()
     context = {"elections": elections}
     return render(request, "elections/elections/elections.html", context)
 
-
+@login_required(login_url='login')
 def electionDetails(request, election_id):
     election = Election.objects.get(election_id=election_id)
     categories = Category.objects.filter(election=election_id)
     context = {"election": election, "categories": categories}
     return render(request, "elections/elections/election_details.html", context)
 
-
+@login_required(login_url='login')
 def categoryDetails(request, category_id):
     category = Category.objects.get(category_id=category_id)
     candidates = Candidate.objects.filter(candidate_category=category_id)
@@ -38,7 +68,6 @@ def categoryDetails(request, category_id):
     
     election = category.election
 
-    # Does not work
     if request.method == 'POST':
         candidate = Candidate.objects.get(candidate_id=request.POST.get('candidate_id'))
 
@@ -49,7 +78,7 @@ def categoryDetails(request, category_id):
         vote = Vote.objects.create(voter_id=voter, candidate_id=candidate, election_id=election)
         vote.save()
             
-        return redirect('category_details', category_id=category_id)
+        return redirect('elections_details', category_id=category_id)
 
     context = {"category": category, "candidates": candidates}
     return render(request, "elections/elections/category.html", context)
