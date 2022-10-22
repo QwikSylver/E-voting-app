aa2e31fd7e620e4f2415dc35e3aede5fb3412056
Profiliu <muaprof2@gmail.com>
2022-10-22 03:16:04 +0200

    Added urls and view
    Moved template views into elections folder

--- app/elections/urls.py
+++ app/elections/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView, UserProfileView
 
 
 urlpatterns = [
     path("", HomePageView, name="home"),
+    path("user_profile/<str:user_id>", UserProfileView, name="user_profile"),
 ]
